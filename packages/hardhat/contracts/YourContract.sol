7862ed7e81cc59f1029024b25af2527d674cb928
giorgosn <sdi1400127@di.uoa.gr>
2021-10-17 02:34:18 +0300

    add erc20 and erc721 contracts from open zepeling
    based on the wildcard project start moving the smart contracts

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,14 +2,14 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
 
   //event SetPurpose(address sender, string purpose);
 
   string public purpose = "Building Unstoppable Apps!!!";
-
+  mapping(uint256 => uint256) public price; //in wei
   constructor() {
     // what should we do on deploy?
   }
